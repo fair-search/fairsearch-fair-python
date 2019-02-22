1af847ed4c70373a81b8c48a7fea1f925d0ed6ee
Ivan Kitanovski <ivan.kitanovski@gmail.com>
2019-02-22 20:37:05 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,41 @@
+import sys
 from setuptools import setup
+
+setup_requires = (
+    ['pytest-runner'] if any(x in sys.argv for x in ('pytest', 'test', 'ptr')) else []
+)
 
 setup(
     name='fairsearchcore',
     version='0.0.2',
     description='A Python library with the core algorithms used to do fair search. ',
-    license='The Apache Software License, Version 2.0',
+    license='Apache 2.0',
     packages=['fairsearchcore'],
     author='Ivan Kitanovski',
     author_email='ivan.kitanovski@gmail.com',
+    url='https://github.com/fair-search/fairsearchcore-python',
     keywords=['search','fairness', 'fa*ir', 'ranking', 'reranking'],
     python_requires=">=3.0",
-    url='https://github.com/fair-search/fairsearch-core',
+    install_requires=[
+        'pandas>=0.23',
+        'scipy>=1.1.0',
+    ],
+    tests_require=[
+        'pytest>=2.8.0'
+    ],
+    setup_requires=setup_requires,
+    test_suite="tests",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: PyPy'
+      ]
 )
